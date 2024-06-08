ea0749bdf372caa3cc142d0e7c15890e72ed0f4b
Jerry Tsai <jerry_tsai@taogo.com.tw>
2024-06-08 16:43:46 -0700

    fixed route

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -1,15 +1,16 @@
 const User = require("../../../models/user");
 
-export default async function route(req, res) {
-  if (req.method === "GET") {
-    const user = req.user;
-    res.status(200).json(user);
-  }
-  if (req.method === "POST") {
-    console.log("h");
-    const { firstname, lastname, username, password, email } = req.body;
-    const user = new User({ username, email, firstname, lastname });
-    const registeredUser = await User.register(user, password);
-    res.redirect("/");
-  }
+export async function GET(req) {
+  const user = await req.user.json();
+  return Response.json(user, { status: 200 });
+}
+
+export async function POST(req) {
+  const body = await req.body.json();
+  console.log("body", body);
+  const { firstname, lastname, username, password, email } = body;
+  const user = new User({ username, email, firstname, lastname });
+  await User.register(user, password);
+  redirect("/");
+  return new Response("User created successfully", { status: 200 });
 }
