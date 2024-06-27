9bb72d7445339cc4cc1561ba2cda5745ad4fde4f
Leigh Yang <leigh79712@hotmail.com>
2024-06-27 14:23:18 -0700

    update form

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -30,16 +30,3 @@ export async function POST(request) {
     data,
   });
 }
-// export default async function route(req, res) {
-//   if (req.method === "GET") {
-//     const user = req.user;
-//     res.status(200).json(user);
-//   }
-//   if (req.method === "POST") {
-//     console.log("h");
-//     const { firstname, lastname, username, password, email } = req.body;
-//     const user = new User({ username, email, firstname, lastname });
-//     const registeredUser = await User.register(user, password);
-//     res.redirect("/");
-//   }
-// }
